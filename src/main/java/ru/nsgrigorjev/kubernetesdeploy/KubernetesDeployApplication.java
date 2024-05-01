654d7d42debbdc604740ca7239d01c7453906a88
Nick Grigorjev <ns.grigorjev@gmail.com>
2024-05-01 10:15:52 +0400

    Initial commit

---
+++ src/main/java/ru/nsgrigorjev/kubernetesdeploy/KubernetesDeployApplication.java
@@ -0,0 +1,13 @@
+package ru.nsgrigorjev.kubernetesdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesDeployApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/nsgrigorjev/kubernetesdeploy/controller/SomeController.java
@@ -0,0 +1,2 @@
+package ru.nsgrigorjev.kubernetesdeploy.controller;public class SomeController {
+}
